0804bbe53339dc6442b4e0dc3a7b4664a378b55e
unknown <isaacbambaz2#gmail.com>
2025-12-10 16:18:53 -0300

    Ajuste saucedemo

--- cypress/e2e/Login/saucedemo.cy.js
+++ cypress/e2e/Login/saucedemo.cy.js
@@ -3,26 +3,15 @@
 describe('Testar site Saucedemo', () => {
    it('Adicionar um item ao carrinho e verificar se tem o numeral 1 aparecendo no carrinho', () => {
 
-cy.visit('https://www.saucedemo.com/')
-
+      cy.visit('https://www.saucedemo.com/')
 
       cy.get('[data-test="username"]').type('standard_user')
-
-
       cy.get('[data-test="password"]').type('secret_sauce')
-
-
       cy.get('[data-test="login-button"]').click()
-
-
       cy.get('[data-test="title"]').should('have.text', 'Products')
-
-
       cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-
-
       cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
-      
+
    });
 
    it('Adicionar um item ao carrinho e verificar se tem o item adicionado está no carrinho', () => {
